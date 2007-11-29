46a76b4934336da6575f95a65edc0af71d431f91
noel.elissa <noel.elissa@b439b9d8-ef3e-0410-9a9d-c578e621f58f>
2007-11-29 05:41:11 +0000

    added simple_go1 folder and python files.

---
+++ simple_go1/simple_go.js
@@ -0,0 +1,16 @@
+var EMPTY = ".";
+var BLACK = "X";
+var WHITE = "O";
+var WORST_SCORE = -1000000000;
+var normal_stone_value_ratio = 0.7;
+var x_coords_string = "ABCDEFGHJKLMNOPQRSTUVWXYZ";
+var max_size = len(x_coords_string);
+var debug_flag = false;
+
+//BOTH = BLACK + WHITE
+
+//colors = [BLACK, WHITE]
+
+//other_side = {BLACK: WHITE, WHITE: BLACK}
+
+//PASS_MOVE = (-1, -1)
