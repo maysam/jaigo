4741303b5557527c0a34675b9ac4a3b24e8070e3
noel.elissa <noel.elissa@b439b9d8-ef3e-0410-9a9d-c578e621f58f>
2007-11-30 05:21:35 +0000

    added a couple of functions that don't work.

--- trunk/simple_go1/simple_go.js
+++ trunk/simple_go1/simple_go.js
@@ -7,10 +7,37 @@ var x_coords_string = "ABCDEFGHJKLMNOPQRSTUVWXYZ";
 var max_size = len(x_coords_string);
 var debug_flag = false;
 
-//BOTH = BLACK + WHITE
+var BOTH = BLACK + WHITE;
 
-//colors = [BLACK, WHITE]
+colors = {BLACK, WHITE}
 
-//other_side = {BLACK: WHITE, WHITE: BLACK}
+other_side = {BLACK: WHITE, WHITE: BLACK}
 
-//PASS_MOVE = (-1, -1)
+PASS_MOVE = {-1, -1}
+
+function move_as_string(move, board_size)
+{
+    /*convert move tuple to string
+      example: (2, 3) -> B3
+	*/
+    if (move==PASS_MOVE)
+ 	{
+		return "PASS";
+	}
+    x, y = move;
+    return x_coords_string[x-1] + str(y);
+}
+
+function string_as_move(m, size)
+{
+    /*convert string to move tuple
+      example: B3 -> (2, 3)
+    */
+    if (m=="PASS")
+	{
+		return PASS_MOVE;
+	}
+    x = string.find(x_coords_string, m[0]) + 1;
+    y = int(m[1:]);
+    return x,y;
+}
