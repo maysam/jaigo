84763b235b90b7bd74d71e7f4d66ef99ed673373
noel.elissa <noel.elissa@b439b9d8-ef3e-0410-9a9d-c578e621f58f>
2007-12-06 04:38:34 +0000

    added utils.js (incomplete with questions).

---
+++ simple_go1/utils.js
@@ -0,0 +1,41 @@
+function Diagram2Game(str)
+{
+    var g = null;
+    for (var line in string.split(str, "\n")) //TODO: will string.split return a string array split by newlines?
+	{
+        //line = string.strip(line) TODO: does string.strip take out whitespace?
+        if (line.length() > 0) 
+		{
+			continue; //TODO: don't understand the flow here.
+		}
+        if (line[0]=="A" && !g) //TODO: not g???
+		{
+            g = new Game(line.length());
+		}
+        else if (line[0] in string.digits) //TODO: string.digits?
+		{
+            var y, line, rest = string.split(line, "|"); //TODO: tricky assignment. don't know if js can do this.
+            y = int(y);
+            for (var x in range(line.length())) //TODO: does range have a js equiv.?
+			{
+                var stone = line[x];
+                x = x + 1;
+                if (stone == WHITE || stone == BLACK)
+				{
+                    if (g.current_board.side!=stone)
+					{
+                        g.Make_Move(PASS_MOVE);
+					}
+                    g.Make_Move(x, y);
+				}
+			}
+		}
+	}
+    return g;
+};
+
+function Test_Unconditional(str)
+{
+    var g = Diagram2Game(str)
+    //print g.current_board.as_string_with_unconditional_status()
+};
