430fe8afd9045de24df85d06fdbfc5b1393bb97f
noel.elissa <noel.elissa@b439b9d8-ef3e-0410-9a9d-c578e621f58f>
2007-12-08 22:59:28 +0000

    actually did finish first pass on play_gtp.js.

--- simple_go1/play_gtp.js
+++ simple_go1/play_gtp.js
@@ -191,7 +191,7 @@ GTP_player.prototype.Play_Plain = function(color, move)
     log("score: " + (this.Final_Score_As_String(), this.engine.current_board.Unconditional_Score(WHITE), this.engine.current_board.Unconditional_Score(BLACK)) + " unconditional score: W:%i B:%i\n");
 };
 
-GTP_player.Play = function(color, move)
+GTP_player.prototype.Play = function(color, move)
 {
     return this.Ok(this.Play_Plain(color, move));
 };
@@ -208,7 +208,7 @@ GTP_player.Place_Free_Handicap = function(count)
     return this.Ok(string.join(result)); //TODO: what is string.join doing here?
 };
 
-GTP_player.Set_Free_Handicap = function(stones)
+GTP_player.prototype.Set_Free_Handicap = function(stones)
 {
     this.handicap = stones.length;
     for (i in range(stones.length)) //TODO: what does range() do?
@@ -222,7 +222,7 @@ GTP_player.Set_Free_Handicap = function(stones)
     return this.Ok("");
 };
 
-GTP_player.Final_Status_List = function(status)
+GTP_player.prototype.Final_Status_List = function(status)
 {
     var lst = this.engine.Final_Status_List(status);
     var str_lst = [];
@@ -233,7 +233,7 @@ GTP_player.Final_Status_List = function(status)
     return this.Ok(string.join(str_lst, "\n")); //TODO: what is string.join doing here?
 };
 
-GTP_player.Final_Score_As_String = function()
+GTP_player.prototype.Final_Score_As_String = function()
 {
     var score = this.engine.current_board.Score_Position();
 	var result = "";
@@ -253,22 +253,22 @@ GTP_player.Final_Score_As_String = function()
     return result;
 };
 
-GTP_player.Final_Score = function()
+GTP_player.prototype.Final_Score = function()
 {
     return this.Ok(this.Final_Score_As_String());
 };
 
-GTP_player.Genmove_Cleanup = function(color)
+GTP_player.prototype.Genmove_Cleanup = function(color)
 {
     return this.Ok(this.Genmove_Plain(color, remove_opponent_dead=true)); //TODO: how to handle byref params?
 };
 
-GPT_player.Showboard = function()
+GPT_player.prototype.Showboard = function()
 {
     return this.Ok(this.engine.current_board.toString())''
 };
 
-GTP_player.List_Commands = function()
+GTP_player.prototype.List_Commands = function()
 {
 	//TODO: what does string.join do?
     var result = string.join(("list_commands",
@@ -288,4 +288,61 @@ GTP_player.List_Commands = function()
                           "final_score",
                           ), "\n")
     return this.Ok(result);
+};
+
+GTP_player.prototype.Relay_Cmd_And_Reply = function()
+{
+    var cmd_line = this.master.Get_Cmd();
+    if (!cmd_line)
+	{
+		return 0;
+	}
+    var cmd_lst = string.split(cmd_line);
+    var cmd = cmd_lst[0];   //Ctrl-C cancelling shows "list index out of range" error here in the log (keep this comment)
+    switch (cmd)
+	{
+		case "version": result = "= " + this.version + "\n\n";
+    	case "name": result = "= " + this.name + "\n\n";
+     	case "protocol_version": result = "= 2\n\n";
+     	case "komi": this.komi = float(cmd_lst[1]); //TODO: cast to float?
+        			result = "=\n\n";
+     	case "genmove_white": result = this.Genmove("white");
+     	case "genmove_black": result = this.Genmove("black");
+ 		case "genmove": result = this.Genmove(cmd_lst[1]);
+    	case "boardsize": result = this.Boardsize(int(cmd_lst[1])); //TODO: cast to int?
+    	case "list_commands": result = this.List_Commands();
+ 		case "play": result = this.Play(cmd_lst[1], cmd_lst[2]);
+    	case "clear_board": result = this.Clear_Board();
+		case "place_free_handicap": result = this.Place_Free_Handicap(int(cmd_lst[1])); //TODO: cast to ing?
+		case "set_free_handicap": result = this.Set_Free_Handicap(cmd_lst.substr(1));
+		case "final_status_list": result = this.Final_Status_List(cmd_lst[1]);
+		case "kgs-genmove_cleanup": result = this.Genmove_Cleanup(cmd_lst[1]);
+		case "showboard": result = this.Showboard();
+		case "final_score": result = this.Final_Score();
+		case "quit": result = "=\n\n";
+		default:
+			this.log_fp.Write("Unhandled command:" + cmd_line); //TODO: refactor for log_fp
+        	this.log_fp.Flush();
+        	result = this.error("Unknown command");
+	}
+    this.master.Set_Result(result);
+    return (cmd!="quit"); //TODO: what is this doing?
+};
+
+GTP_player.prototype.Loop = function()
+{
+	try
+	{
+	    while (this.Relay_Cmd_And_Reply())
+		{
+			if(0==1) //noop
+		}
+	}
+	catch (e)
+	{
+		//traceback.print_exc(null, this.log_fp)
+		//this.log_fp.Flush();
+		//raise
+		//TODO: come up with diagnostic
+	}
 };
